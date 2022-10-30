79299487d656e2719e2ccee42b1a606c472d3b7b
ColonelKai <kayraacar@hotmail.co.jp>
2022-10-31 00:14:50 +0300

    init

---
+++ src/main/java/econtoolbox/economist/Economist.java
@@ -0,0 +1,17 @@
+package econtoolbox.economist;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Economist extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
