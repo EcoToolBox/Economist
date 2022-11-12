9e4087562fb0c0d4898c761c6393cfd76c2af4b7
ColonelKai <kayraacar@hotmail.co.jp>
2022-11-12 17:36:33 +0300

    Create Manager's Data File if missing on startup.

--- src/main/java/econtoolbox/economist/managers/AbstractManager.java
+++ src/main/java/econtoolbox/economist/managers/AbstractManager.java
@@ -3,13 +3,38 @@ package econtoolbox.economist.managers;
 import econtoolbox.economist.Economist;
 import econtoolbox.economist.misc.Files;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.StandardCopyOption;
+
+import static econtoolbox.economist.Economist.getPlugin;
 
 public abstract class AbstractManager implements Manager {
     File file;
 
-    public void load() {
+    public void prepare() {
         this.file = new File(Files.DATA_FOLDER, this.getManagerName() + ".yml");
+        if(!this.file.exists()) {
+            this.file.getParentFile().mkdir();
+            InputStream defaultFile = Economist.getPlugin().getResource(this.file.getName());
+
+            // I have no idea what I'm doing here.
+            try {
+                assert defaultFile != null;
+                java.nio.file.Files.copy(
+                        defaultFile,
+                        this.file.toPath(),
+                        StandardCopyOption.REPLACE_EXISTING
+                );
+            } catch (IOException e) {
+                Economist.getPlugin().getLogger().warning("Error during the creation of default file for manager: " + this.getManagerName());
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+    public void load() {
+        this.prepare();
     }
 
     @Override
